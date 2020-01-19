fc1a8da9684f64a210b2b3bdea5890109cf1a4cf
Dmitry Koltovich <steklopod@gmail.com>
2020-01-19 18:32:21 +0300

    Java 11

--- config/src/main/java/com/piggymetrics/ConfigApplication.java
+++ config/src/main/java/com/piggymetrics/ConfigApplication.java
@@ -1,4 +1,4 @@
-package com.piggymetrics.config;
+package com.piggymetrics;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- config/src/main/java/com/piggymetrics/config/SecurityConfig.java
+++ config/src/main/java/com/piggymetrics/config/SecurityConfig.java
@@ -11,7 +11,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    public void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
         http
             .authorizeRequests()
